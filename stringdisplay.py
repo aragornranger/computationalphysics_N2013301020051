1630b770bc29d6b8791c466d4585452aec527ce3
aragornranger <biruike@gmail.com>
2016-03-13 14:36:35 +0800

    stringdisplay.py can check if the input is legal now

--- stringdisplay.py
+++ stringdisplay.py
@@ -27,12 +27,25 @@ cap =[['  *  ',' * * ','*****','*   *','*   *'],
  
 #initialization
 
-letters = raw_input("The letters you want to print:\n")
-#input the letters you want to diplay
-letters = letters.upper()
-#can only display captial letters
+legal = 0
+
+while legal == 0:
+
+    legal = 1
+    letters = raw_input("The letters you want to print:\n")
+    #input the letters you want to diplay
+    letters = letters.upper()
+    #can only display captial letters
+
+    for i in letters:
+        if (ord(i) != ord(' ')) and (not (ord('A') <= ord(i) <= ord('Z')) and not(ord('a') <= ord(i) <= ord('z'))):
+            legal = 0
+            print('Error!Only letters and spaces can be displayed.')
+            break;
+    #check if all chars input are letters or spaces
 
 output = ['','','','','']
+
 
 for i in letters:
     for j in range(5):
